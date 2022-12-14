0f7386fb356668a8594832cc33668df140d54961
Prohor <prohor-yakovlev@mail.ru>
2022-12-14 13:29:47 +0300

    copyObject

--- 62_1/main.js
+++ 62_1/main.js
@@ -14,10 +14,42 @@ const user2 = user1
 user2.name = "Ann"
 console.log(user1)
 
-const copyUser = {} // new Object()
-copyUser.name = user1.name
-copyUser.age = user1.age
-copyUser.isStudent = user1.isStudent
+// const copyUser = {} // new Object()
+// copyUser.name = user1.name
+// copyUser.age = user1.age
+// copyUser.isStudent = user1.isStudent
+
+const copyUser = {...user1}
 
 console.log(copyUser)
-console.log(copyUser === user2)
+console.log(copyUser === user2) // false
+
+const copyUser1 = {...user1, isStudent: false}// изменения существующего св-ва
+const copyUser2 = {...user1, isMarried: false}// добавление св-ва
+
+const address = {
+    country: "Thailand",
+    city: "Bangkok"
+}
+const copyUser3 = {...user1, ...address}
+console.log(copyUser3)
+// глубокое копирование
+const student = {
+    name: "Bob",
+    age: 23,
+    isStudent: true,
+    address: {  //#789
+        country: "Thailand",
+        city: "Bangkok"
+    },
+    courses: ["HTML", "CSS", "JS"]//#790
+}
+//
+const copyStudent = {...student}
+copyStudent.courses.push("REACT")
+console.log(student)
+
+const copyStudent2 = {...student, courses: [...student.courses, "Redux"]}
+
+console.log(student.courses)
+console.log(copyStudent2)
