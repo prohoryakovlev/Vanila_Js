488ad15a35bee3fd090bc3983226406a0ba8020d
Prohor <prohor-yakovlev@mail.ru>
2023-01-04 19:09:35 +0300

    project

--- 62_3/main.js
+++ 62_3/main.js
@@ -1,5 +1,8 @@
+// Есть ли обработчик? (function) => Handler, Listener, Subscriber
+// При вызове формируется объект  ({}) => event, ev, e
+// Если есть хэндлер => function() => callback
 
-
+const sm = document.getElementById("small")
 
 
 
